fa0f1a81aff7db77d79e5a0204e4b6d1a829b6cf
crystian97 <icrystianveloso@gmail.com>
2023-03-23 00:17:20 -0300

    feat:create start screen

--- src/Pages/Start/index.jsx
+++ src/Pages/Start/index.jsx
@@ -1,3 +1,20 @@
+import { Link } from "react-router-dom";
+
 export default function Start() {
-  return <h1> Start</h1>;
+  return (
+    <div className="bg-gray-800 h-screen flex flex-row justify-center py-8 pr-32 w-full">
+      <div className="flex flex-col gap-2 mr-64">
+        <img src="./naruto-ultimate-quotes-logo.png" alt="" />
+        <img src="./chars.png" alt="" />
+      </div>
+      <div className="flex flex-col align-middle justify-center">
+        <Link
+          to="/home"
+          className="border-orange-400 w-96 h-20 bg-white text-3xl text-orange-400 flex justify-center items-center border-solid border-4"
+        >
+          Start
+        </Link>
+      </div>
+    </div>
+  );
 }
